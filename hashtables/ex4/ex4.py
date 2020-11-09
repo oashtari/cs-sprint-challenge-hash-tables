859e8dff737a928fbb1693bf032d1232f0a8adee
Omid Ashtari <oashtari@gmail.com>
2020-11-08 18:34:41 -0800

    finished ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,36 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    twins = {}
+    result = {}
 
-    return result
+    for number in a:
+        twins[number] = number
+        # print('twins', twins)
+
+    for k,v in twins.items():
+        # print('k', k)
+        if k * -1 in twins:
+            result[abs(k)] = 1
+            # print('result', result)
+
+    result_array = []
+
+    if result == {}:
+        return []
+
+    for item in result.items():
+        # print('result', result)
+        # print('item', item)
+        if item[0] != 0:
+            result_array.append(item[0])
+    
+    return result_array
+
+    # for i in range(len(a)):
+    #     if a[i] > 0:
+    #         twins[a[i]] = a[i]
+    #     else:
 
 
 if __name__ == "__main__":
